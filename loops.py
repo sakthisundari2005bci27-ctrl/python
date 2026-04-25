07ec08d82d91fae22766de1d2f03be6e043f8d36
sakthisundari2005bci27-ctrl <sakthisundari2005_bci27@mepcoeng.ac.in>
2026-04-25 10:29:32 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    # Iterate through all non-negative integers less than n
+    for i in range(n):
+        # Print the square of the number
+        print(i**2)
